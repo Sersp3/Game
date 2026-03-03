16e167089bcecbc341a62c8b003073d1f650391a
Sergey <pavlikov.ss@phystech.edu>
2026-03-03 12:39:27 +0300

    start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,16 @@
 
 if [[ ! -f "build/CMakeCache.txt" ]]; then
   cmake -B build
+  if [[ "$?" != 0 ]]; then
+    echo "Program didn't pass setting the enviroment\n"
+    exit 1 
+
 fi
 
 cmake --build build
 if [[ "$?" == 0 ]]; then
   ./build/bin/main
+else; then
+  echo "Programm didn't pass the build\n"
+  exit 1
 fi
